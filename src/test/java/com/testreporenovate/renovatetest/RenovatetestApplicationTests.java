85d1ade263757aad9b4b876ce4538c570794a6fd
tofferman <tofferman@bol.com>
2022-12-14 21:43:53 +0100

    main: init for maven

---
+++ src/test/java/com/testreporenovate/renovatetest/RenovatetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testreporenovate.renovatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovatetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
